fdd07b6239c5d78086bad2ab4ba61a8fa4c1b5dd
Francisco García Sierra <francisco.garciasierra@bq.com>
2017-04-11 23:02:39 +0200

    Added javadoc for RxDatabase, RxAuth adn RxChildEvents classes

--- app/src/main/java/durdinapps/rxfirebase2/RxFirebaseDatabase.java
+++ app/src/main/java/durdinapps/rxfirebase2/RxFirebaseDatabase.java
@@ -32,6 +32,13 @@ import io.reactivex.functions.Function;
 
 public class RxFirebaseDatabase {
 
+   /**
+    * Listener for changes in te data at the given query location.
+    *
+    * @param query    reference represents a particular location in your Database and can be used for reading or writing data to that Database location.
+    * @param strategy {@link BackpressureStrategy} associated to this {@link Flowable}
+    * @return a {@link Flowable} which emits when a value of the database change in the given query.
+    */
    @NonNull
    public static Flowable<DataSnapshot> observeValueEvent(@NonNull final Query query,
                                                           @NonNull BackpressureStrategy strategy) {
@@ -60,7 +67,12 @@ public class RxFirebaseDatabase {
       }, strategy);
    }
 
-
+   /**
+    * Listener for a single change in te data at the given query location.
+    *
+    * @param query reference represents a particular location in your Database and can be used for reading or writing data to that Database location.
+    * @return a {@link Maybe} which emits the actual state of the database for the given query.
+    */
    @NonNull
    public static Maybe<DataSnapshot> observeSingleValueEvent(@NonNull final Query query) {
       return Maybe.create(new MaybeOnSubscribe<DataSnapshot>() {
@@ -84,6 +96,14 @@ public class RxFirebaseDatabase {
       });
    }
 
+   /**
+    * Run a transaction on the data at this location. For more information on running transactions, see
+    *
+    * @param ref              reference represents a particular location in your database.
+    * @param fireLocalEvents  boolean which allow to receive calls of your transaction in your local device.
+    * @param transactionValue value of the transaction.
+    * @return a {@link Single} which emits the final {@link DataSnapshot} value if the transaction success.
+    */
    @NonNull
    public static Single<DataSnapshot> runTransaction(@NonNull final DatabaseReference ref,
                                                      @NonNull final boolean fireLocalEvents,
@@ -104,13 +124,24 @@ public class RxFirebaseDatabase {
 
                @Override
                public void onComplete(DatabaseError databaseError, boolean b, DataSnapshot dataSnapshot) {
-                  emitter.onSuccess(dataSnapshot);
+                  if (databaseError != null) {
+                     emitter.onError(new RxFirebaseDataException(databaseError));
+                  } else {
+                     emitter.onSuccess(dataSnapshot);
+                  }
                }
             }, fireLocalEvents);
          }
       });
    }
 
+   /**
+    * Update the specific child keys to the specified values.
+    *
+    * @param ref reference represents a particular location in your database.
+    * @param updateData The paths to update and their new values
+    * @return a {@link Completable} which is complete when the update children call finish successfully.
+    */
    @NonNull
    public static Completable updateChildren(@NonNull final DatabaseReference ref,
                                             @NonNull final Map<String, Object> updateData) {
@@ -131,6 +162,13 @@ public class RxFirebaseDatabase {
       });
    }
 
+   /**
+    * Listener for for child events occurring at the given query location.
+    *
+    * @param query    reference represents a particular location in your Database and can be used for reading or writing data to that Database location.
+    * @param strategy {@link BackpressureStrategy} associated to this {@link Flowable}
+    * @return a {@link Flowable} which emits when a value of a child int the database change on the given query.
+    */
    @NonNull
    public static Flowable<RxFirebaseChildEvent<DataSnapshot>> observeChildEvent(
       @NonNull final Query query, @NonNull BackpressureStrategy strategy) {
@@ -142,27 +180,27 @@ public class RxFirebaseDatabase {
                @Override
                public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
                   emitter.onNext(
-                     new RxFirebaseChildEvent<DataSnapshot>(dataSnapshot.getKey(), dataSnapshot, previousChildName,
+                     new RxFirebaseChildEvent<>(dataSnapshot.getKey(), dataSnapshot, previousChildName,
                         RxFirebaseChildEvent.EventType.ADDED));
                }
 
                @Override
                public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
                   emitter.onNext(
-                     new RxFirebaseChildEvent<DataSnapshot>(dataSnapshot.getKey(), dataSnapshot, previousChildName,
+                     new RxFirebaseChildEvent<>(dataSnapshot.getKey(), dataSnapshot, previousChildName,
                         RxFirebaseChildEvent.EventType.CHANGED));
                }
 
                @Override
                public void onChildRemoved(DataSnapshot dataSnapshot) {
-                  emitter.onNext(new RxFirebaseChildEvent<DataSnapshot>(dataSnapshot.getKey(), dataSnapshot,
+                  emitter.onNext(new RxFirebaseChildEvent<>(dataSnapshot.getKey(), dataSnapshot,
                      RxFirebaseChildEvent.EventType.REMOVED));
                }
 
                @Override
                public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
                   emitter.onNext(
-                     new RxFirebaseChildEvent<DataSnapshot>(dataSnapshot.getKey(), dataSnapshot, previousChildName,
+                     new RxFirebaseChildEvent<>(dataSnapshot.getKey(), dataSnapshot, previousChildName,
                         RxFirebaseChildEvent.EventType.MOVED));
                }
 
@@ -183,6 +221,14 @@ public class RxFirebaseDatabase {
       }, strategy);
    }
 
+   /**
+    * Listener for changes in te data at the given query location.
+    *
+    * @param query    reference represents a particular location in your Database and can be used for reading or writing data to that Database location.
+    * @param clazz    class type for the {@link DataSnapshot} items.
+    * @param strategy {@link BackpressureStrategy} associated to this {@link Flowable}
+    * @return a {@link Flowable} which emits when a value of the database change in the given query.
+    */
    @NonNull
    public static <T> Flowable<T> observeValueEvent(@NonNull final Query query,
                                                    @NonNull final Class<T> clazz,
@@ -190,13 +236,27 @@ public class RxFirebaseDatabase {
       return observeValueEvent(query, DataSnapshotMapper.of(clazz), strategy);
    }
 
-
+   /**
+    * Listener for a single change in te data at the given query location.
+    *
+    * @param query reference represents a particular location in your Database and can be used for reading or writing data to that Database location.
+    * @param clazz class type for the {@link DataSnapshot} items.
+    * @return a {@link Maybe} which emits the actual state of the database for the given query.
+    */
    @NonNull
    public static <T> Maybe<T> observeSingleValueEvent(@NonNull final Query query,
                                                       @NonNull final Class<T> clazz) {
       return observeSingleValueEvent(query, DataSnapshotMapper.of(clazz));
    }
 
+   /**
+    * Listener for for child events occurring at the given query location.
+    *
+    * @param query    reference represents a particular location in your Database and can be used for reading or writing data to that Database location.
+    * @param clazz    class type for the {@link DataSnapshot} items.
+    * @param strategy {@link BackpressureStrategy} associated to this {@link Flowable}
+    * @return a {@link Flowable} which emits when a value of a child int the database change on the given query.
+    */
    @NonNull
    public static <T> Flowable<RxFirebaseChildEvent<T>> observeChildEvent(
       @NonNull final Query query, @NonNull final Class<T> clazz,
@@ -204,6 +264,14 @@ public class RxFirebaseDatabase {
       return observeChildEvent(query, DataSnapshotMapper.ofChildEvent(clazz), strategy);
    }
 
+   /**
+    * Listener for changes in te data at the given query location.
+    *
+    * @param query    reference represents a particular location in your Database and can be used for reading or writing data to that Database location.
+    * @param mapper   specific function to map the dispatched events.
+    * @param strategy {@link BackpressureStrategy} associated to this {@link Flowable}
+    * @return a {@link Flowable} which emits when a value of the database change in the given query.
+    */
    @NonNull
    public static <T> Flowable<T> observeValueEvent(@NonNull final Query query,
                                                    @NonNull final Function<? super DataSnapshot, ? extends T> mapper,
@@ -211,12 +279,27 @@ public class RxFirebaseDatabase {
       return observeValueEvent(query, strategy).map(mapper);
    }
 
+   /**
+    * Listener for a single change in te data at the given query location.
+    *
+    * @param query  reference represents a particular location in your Database and can be used for reading or writing data to that Database location.
+    * @param mapper specific function to map the dispatched events.
+    * @return a {@link Maybe} which emits the actual state of the database for the given query.
+    */
    @NonNull
    public static <T> Maybe<T> observeSingleValueEvent(@NonNull final Query query,
                                                       @NonNull final Function<? super DataSnapshot, ? extends T> mapper) {
       return observeSingleValueEvent(query).map(mapper);
    }
 
+   /**
+    * Listener for for child events occurring at the given query location.
+    *
+    * @param query    reference represents a particular location in your Database and can be used for reading or writing data to that Database location.
+    * @param mapper   specific function to map the dispatched events.
+    * @param strategy {@link BackpressureStrategy} associated to this {@link Flowable}
+    * @return a {@link Flowable} which emits when a value of a child int the database change on the given query.
+    */
    @NonNull
    public static <T> Flowable<RxFirebaseChildEvent<T>> observeChildEvent(
       @NonNull final Query query, @NonNull final Function<? super RxFirebaseChildEvent<DataSnapshot>,
@@ -224,42 +307,88 @@ public class RxFirebaseDatabase {
       return observeChildEvent(query, strategy).map(mapper);
    }
 
+   /**
+    * Listener for changes in the data at the given query location.
+    *
+    * @param query reference represents a particular location in your Database and can be used for reading or writing data to that Database location.
+    * @return a {@link Flowable} which emits when a value of the database change in the given query.
+    */
    @NonNull
    public static Flowable<DataSnapshot> observeValueEvent(@NonNull final Query query) {
       return observeValueEvent(query, BackpressureStrategy.DROP);
    }
 
 
+   /**
+    * Listener for for child events occurring at the given query location.
+    *
+    * @param query reference represents a particular location in your Database and can be used for reading or writing data to that Database location.
+    * @return a {@link Flowable} which emits when a value of a child int the database change on the given query.
+    */
    @NonNull
    public static Flowable<RxFirebaseChildEvent<DataSnapshot>> observeChildEvent(
       @NonNull final Query query) {
       return observeChildEvent(query, BackpressureStrategy.DROP);
    }
 
+   /**
+    * Run a transaction on the data at this location. For more information on running transactions, see
+    *
+    * @param ref              reference represents a particular location in your database.
+    * @param transactionValue value of the transaction.
+    * @return a {@link Single} which emits the final {@link DataSnapshot} value if the transaction success.
+    */
    @NonNull
    public static Single<DataSnapshot> runTransaction(@NonNull final DatabaseReference ref,
                                                      @NonNull final long transactionValue) {
       return runTransaction(ref, true, transactionValue);
    }
 
+   /**
+    * Listener for changes in te data at the given query location.
+    *
+    * @param query reference represents a particular location in your Database and can be used for reading or writing data to that Database location.
+    * @param clazz class type for the {@link DataSnapshot} items.
+    * @return a {@link Flowable} which emits when a value of the database change in the given query.
+    */
    @NonNull
    public static <T> Flowable<T> observeValueEvent(@NonNull final Query query,
                                                    @NonNull final Class<T> clazz) {
       return observeValueEvent(query, DataSnapshotMapper.of(clazz), BackpressureStrategy.DROP);
    }
 
+   /**
+    * Listener for for child events occurring at the given query location.
+    *
+    * @param query reference represents a particular location in your Database and can be used for reading or writing data to that Database location.
+    * @param clazz class type for the {@link DataSnapshot} items.
+    * @return a {@link Flowable} which emits when a value of a child int the database change on the given query.
+    */
    @NonNull
    public static <T> Flowable<RxFirebaseChildEvent<T>> observeChildEvent(
       @NonNull final Query query, @NonNull final Class<T> clazz) {
       return observeChildEvent(query, DataSnapshotMapper.ofChildEvent(clazz), BackpressureStrategy.DROP);
    }
 
+   /**
+    * Listener for changes in te data at the given query location.
+    *
+    * @param query reference represents a particular location in your Database and can be used for reading or writing data to that Database location.
+    * @return a {@link Flowable} which emits when a value of the database change in the given query.
+    */
    @NonNull
    public static <T> Flowable<T> observeValueEvent(@NonNull final Query query,
                                                    @NonNull final Function<? super DataSnapshot, ? extends T> mapper) {
       return observeValueEvent(query, BackpressureStrategy.DROP).map(mapper);
    }
 
+   /**
+    * Listener for for child events occurring at the given query location.
+    *
+    * @param query  reference represents a particular location in your Database and can be used for reading or writing data to that Database location.
+    * @param mapper specific function to map the dispatched events.
+    * @return a {@link Flowable} which emits when a value of a child int the database change on the given query.
+    */
    @NonNull
    public static <T> Flowable<RxFirebaseChildEvent<T>> observeChildEvent(
       @NonNull final Query query, @NonNull final Function<? super RxFirebaseChildEvent<DataSnapshot>,
