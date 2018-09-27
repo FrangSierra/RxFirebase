642571640203225b9be673fb69a25e98bc95b4ef
fragarsie <Bo13choir!>
2018-09-27 15:50:50 +0200

    Update dependencies and add basic implementation for Firebase Cloud Functions

---
+++ app/src/main/java/durdinapps/rxfirebase2/RxFirebaseFunctions.java
@@ -0,0 +1,63 @@
+package durdinapps.rxfirebase2;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.HttpsCallableResult;
+
+import io.reactivex.Single;
+import io.reactivex.SingleEmitter;
+import io.reactivex.SingleOnSubscribe;
+
+public class RxFirebaseFunctions {
+
+    /**
+     * Calls a {@link HttpsCallableResult} which represents a reference to a Google Cloud Functions HTTPS callable function.
+     *
+     * @param functions Instance of {@link FirebaseFunctions}
+     * @param name      Name of the Google Cloud function
+     * @param data      Params for the request.
+     * @return a {@link Single} which will emit the result of the given function.
+     */
+    @NonNull
+    public static Single<HttpsCallableResult> getHttpsCallable(@NonNull final FirebaseFunctions functions,
+                                                               @NonNull final String name,
+                                                               @Nullable final Object data) {
+        return Single.create(new SingleOnSubscribe<HttpsCallableResult>() {
+            @Override
+            public void subscribe(final SingleEmitter<HttpsCallableResult> emitter) {
+                functions.getHttpsCallable(name)
+                    .call(data)
+                    .addOnSuccessListener(new OnSuccessListener<HttpsCallableResult>() {
+                        @Override
+                        public void onSuccess(HttpsCallableResult httpsCallableResult) {
+                            emitter.onSuccess(httpsCallableResult);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            emitter.onError(e);
+                        }
+                    });
+            }
+        });
+    }
+
+
+    /**
+     * Calls a {@link HttpsCallableResult} which represents a reference to a Google Cloud Functions HTTPS callable function.
+     *
+     * @param functions Instance of {@link FirebaseFunctions}
+     * @param name      Name of the Google Cloud function
+     * @return a {@link Single} which will emit the result of the given function.
+     */
+    @NonNull
+    public static Single<HttpsCallableResult> getHttpsCallable(@NonNull final FirebaseFunctions functions,
+                                                               @NonNull final String name) {
+        return getHttpsCallable(functions, name, null);
+    }
+}
